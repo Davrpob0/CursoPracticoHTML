d67fe5293a342e2bf703acd2c73048d853628372
DavidR <davrpob@gmail.com>
2022-10-24 18:24:57 -0500

    se integra el menu completamente

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu")
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const hamIconMenu = document.querySelector(".menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+hamIconMenu.addEventListener("click",toggleMobileMenu)
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle("inactive")
+    desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
