ee3a9aad2cd6bea5a91270369b711bab7750cdb8
DavidR <davrpob@gmail.com>
2022-10-25 20:40:54 -0500

    organizadade estilos

--- main.js
+++ main.js
@@ -50,17 +50,17 @@ const productList = [];
 productList.push({
     name: "Bike",
     price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
 });
 productList.push({
     name: "Laptop",
     price: 340,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image: "https://www.lenovo.com/medias/lenovo-laptop-ideapad-gaming-3-gen-6-15-amd-subseries-hero.png?context=bWFzdGVyfHJvb3R8MTk5NTg0fGltYWdlL3BuZ3xoZmIvaGRiLzE0MTkwNDY2MzY3NTE4LnBuZ3xlMzY3ZDNhMWI3NGI3MjNjZDhkYjQzYWQ2ZjQ0MThiYjg0ZTlhYzUzYzM2MmNhMzVhZmJmOWJiNGE4OThmZTRi0",
 });
 productList.push({
     name: "TV",
     price: 230,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    image: "https://images.samsung.com/is/image/samsung/co-fhd-t5300-un43t5300akxzl-frontblack-237599479?$650_519_PNG$",
 });
 
 function renderProducts(arr) {
@@ -78,10 +78,12 @@ function renderProducts(arr) {
     
         const productPrice = document.createElement("p");
         productPrice.innerText = "$" + product.price;
-    
         const productName = document.createElement("p");
         productName.innerText = product.name;
     
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
         const productInfoFigure = document.createElement("figure");
         const productImgCart = document.createElement("img");
         productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
